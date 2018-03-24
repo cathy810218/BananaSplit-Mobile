6bc7b45fd2718c81a1b9ee5eb2217cb4ea7b2b77
cathy810218 <cathy810218@gmail.com>
2018-03-24 06:01:37 -0700

    Connecting all pages

--- BananaSplit/Controller/CreateAccountViewController.swift
+++ BananaSplit/Controller/CreateAccountViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.backgroundColor = UIColor.white
+        navigationController?.navigationBar.barTintColor = UIColor.white
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.tintColor = UIColor.brown
 
-        // Do any additional setup after loading the view.
+
+        
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        let imageSize = CGSize(width: 80, height: 50)
+        let marginX = ((self.navigationController?.navigationBar.frame.size.width)! / 2) - (imageSize.width / 2);
+        imageView.frame = CGRect(x: marginX, y: 0, width: imageSize.width, height: imageSize.height)
+        self.navigationController?.navigationBar.addSubview(imageView)
+
     }
 
     override func didReceiveMemoryWarning() {
