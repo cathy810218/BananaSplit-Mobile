e85368fbff9a62d69c56bfd509ed5b2e709219bf
Chris Gores <crgores@amazon.com>
2018-03-24 02:23:02 -0700

    Update API calls

--- BananaSplit/Services/API.swift
+++ BananaSplit/Services/API.swift
@@ -11,10 +11,56 @@ import Alamofire
 
 class API {
     func getCredientials() {
-        let params = ["client_id": client_id, "client_secret": client_secret];
-        Alamofire.request("https://api.dxhackathon.com/oauth2/token", method: .get, parameters: params, encoding: JSONEncoding.default, headers: nil).responseJSON { (data) in
-            print(data);
+        let url = "https://api.dxhackathon.com/oauth2/token"
+        let headers = ["Content-Type": "application/x-www-form-urlencoded",
+                       "Accept": "application/json"];
+        let params = ["client_id": client_id,
+                      "client_secret": client_secret,
+                      "grant_type": grant_type];
+        Alamofire.request(
+            url,
+            method: .post,
+            parameters: params,
+            encoding: URLEncoding(destination: .queryString),
+            headers: headers)
+        .responseJSON { (response) in
+            if let JSON = response.result.value as? [String: Any] {
+               let accessToken = JSON["access_token"] as! String
+                print(accessToken)
+                UserDefaults.standard.setValue(accessToken, forKey: "access_token")
+            }
         }
-//        UserDefaults.standard.setValue(token, forKey: "access_token")
+    }
+    
+    func listAccounts(callback: @escaping (String?, [Account]?) -> Void) {
+        let url = "https://api.dxhackathon.com/money-movement/accounts"
+        let accessToken = UserDefaults.standard.string(forKey: "access_token")
+        let headers = ["Accept": "application/json;v=0",
+                       "Authorization": "Bearer " + accessToken!]
+        
+        var accountsArray: [Account] = [];
+        
+        // build and send request
+        Alamofire.request(
+            url,
+            method: .get,
+            parameters: nil,
+            encoding: URLEncoding(destination: .queryString),
+            headers: headers)
+            .responseJSON { (response) in
+                if let JSON = response.result.value as? [String: Any] {
+                    print(response)
+                    if (response.error != nil) {
+                        callback("Error! \(response.description)", nil);
+                    }
+                    
+                    if let accounts = JSON["accounts"] as? [Dictionary<String, Any>] {
+                        for accountObject in accounts {
+                            accountsArray.append(Account(json: accountObject))
+                        }
+                    }
+                    callback(nil, accountsArray);
+                }
+        }
     }
 }
