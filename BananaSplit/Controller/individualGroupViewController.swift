ce0055dedcfb2545f7c254832eddfc9f004a6fd1
Adrian Prananda <prananda0203@gmail.com>
2018-03-24 07:53:34 -0700

    add user data mocks

--- BananaSplit/Controller/individualGroupViewController.swift
+++ BananaSplit/Controller/individualGroupViewController.swift
@@ -11,18 +11,39 @@ import UIKit
 class individualGroupViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
-    
+
+    var users : [User] = []
     var label1 = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         label.text! = label1
+        loadMockData()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func loadMockData() {
+        if let path = Bundle.main.path(forResource: "Users", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as? Dictionary<String, Any>
+                if let users = jsonResult!["users"] as? [Dictionary<String, Any>] {
+                    for user in users {
+                        self.users.append(User(json: ["name": user["name"], "id": user["id"] ?? -1]))
+                    }
+                }
+
+            } catch {
+                // handle error
+            }
+        }
     }
 
     @IBAction func dismissButtonAction(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- BananaSplit/Model/User.swift
+++ BananaSplit/Model/User.swift
@@ -9,7 +9,19 @@
 import Foundation
 
 class User {
-    init() {
-        
+    var json: [String : Any]
+    var id: NSNumber?
+    var name: String?
+    var ammountToPay: NSNumber?
+    var lastFourAccountNumber: NSNumber?
+    init(json: [String: Any]) {
+        self.json = json
+        self.id = json["id"] as? NSNumber
+        self.name = json["name"] as? String
+        self.ammountToPay = 0
+    }
+    
+    var description: String {
+        return "\(json)"
     }
 }
