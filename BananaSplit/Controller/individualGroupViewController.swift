d1e093ccde477a1d5a76c6158166741993106f76
Adrian Prananda <map34@Adrians-4C-MacBook-Pro.local>
2018-03-24 06:22:41 -0700

    dismiss button

--- BananaSplit/Controller/individualGroupViewController.swift
+++ BananaSplit/Controller/individualGroupViewController.swift
@@ -20,6 +20,9 @@ class individualGroupViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func dismissButtonAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
