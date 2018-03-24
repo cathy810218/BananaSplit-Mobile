2402f234762097d0e55c3ae1837097f1ff5deb09
Adrian Prananda <prananda0203@gmail.com>
2018-03-24 04:22:52 -0700

    accounts

---
+++ BananaSplit/View/AccountCell.swift
@@ -0,0 +1,15 @@
+//
+//  AccountCell.swift
+//  BananaSplit
+//
+//  Created by Adrian Prananda on 3/24/18.
+//  Copyright © 2018 Cathy Oun. All rights reserved.
+//
+
+import UIKit
+
+class AccountCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var availableBalance: UILabel!
+}
